5cab4a9114b8fc553ba4d3480296058d0bf197ca
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-07-24 00:57:51 +0530

    add L5

---
+++ Lecture_1-10/04_L5.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //1).Bitwise operator
+
+    // int a = 4;
+    // int b = 6;
+
+    // cout<<"a&b  :" << (a&b)<<endl;
+    // cout<<"a|b  :" << (a|b)<<endl;
+    // cout<<"~a   :" << (~a)<<endl;
+    // cout<<"~b   :" << (~b)<<endl;
+    // cout<<"a^b  :" << (a^b)<<endl;
+
+
+    //2). Right and left shift
+//     cout<< (17>>1)<< endl; // right shift mein 2*1 divde karo
+//     cout<< (17>>2)<< endl; // right shift mein 2*2 divde karo 
+//     cout<< (19<<1)<< endl; // left shift mein 2*1 multiply karo
+//     cout<< (21<<2)<< endl; // left shift mein 2*1 multiply karo
+
+//3).Increment and decrement
+
+// int i=7;
+// cout<< (i++)<< endl;
+// cout<< (++i)<< endl;
+// cout<< (i--)<< endl;
+// cout<< (--i)<< endl;
+
+//4). For loop
+
+// int n;
+// cout<<"Enter thhe value of n:";
+// cin >> n;
+
+// cout <<"Printing count from 1 to 10:";
+
+// for (int i = 0; i < count; i++)
+// {
+//     /* code */
+// }
+
+
+//5). Fibonacci series
+
+// int n=10;
+// int a=0;
+// int b=1;
+
+// cout<<a <<" "<<b<<" ";
+// for (int i = 1; i < n; i++)
+// {
+// int nextNumber = a+b;
+// cout<<nextNumber<<" ";
+
+// a=b;
+// b=nextNumber;
+// }
+
+//break function stop the loop
+//continue skip the loop
+
+//6). Variable scope define
+// int a=3;
+// cout<<a<<endl;
+
+// if(true){
+//     int a=5;
+//     cout<<a<<endl;
+// } //its only define in this scope
+
+// cout<<a<<endl;
+// // output : 3 5 3
+
+
+
+
+
+}
