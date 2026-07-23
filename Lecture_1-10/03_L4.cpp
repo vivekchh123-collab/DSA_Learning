fd44676d1dc85964872870b3e71f9bb0e1ab455c
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-07-23 23:28:13 +0530

    refactor: update and comment out code examples in 03_L4.cpp

--- Lecture_1-10/03_L4.cpp
+++ Lecture_1-10/03_L4.cpp
@@ -20,19 +20,227 @@ int main (){
 // }
 
 //2).
-int n;
-cout<<"Enter the number:"<<endl;
-cin>>n;
+// int n;
+// cout<<"Enter the number:"<<endl;
+// cin>>n;
 
-int i=1;
-while(i<=9){
-    int j=1;
-    while(j<=n){
-        cout<<i<<" ";
-        j++;
-        i++;
-    }
-    cout<<endl;
+// int i=1;
+// int count=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=n){
+//         cout<<count<<" ";
+//         count++;
+//         j++;
+        
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//3).
+
+// int n;
+// cout<<"Enter the number:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=i){
+//         cout<<"*";
+//         j++; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//4).
+// int n;
+// cout<<"Enter the number:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=i){
+//         cout<<i<<" ";
+//         j++; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//5).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// int k=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=i){
+//         cout<<k<<" ";
+//         k++;
+//         j++; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//6).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=1;
+//     int k=0;
+//     while(j<=i){
+//         cout<<k+i<<" ";
+//         k++;
+//         j++; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//7).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=0;
+//     while(j<i){
+//         cout<<i-j;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//8).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// char ch = 'A';
+// while(i<=n){
+//     int j =1;
+//     while (j<=n)
+//     {
+//         cout<<ch;
+//         j++;
+//     }
+//     cout<<endl;
+//     ch++;
+//     i++;
     
-}
+// }
+
+//9).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// char ch ='A';
+// while(i<=n){
+//     int j=1;
+//     char k=ch;
+//     while(j<=n){
+//         cout<<k<<" ";
+//         k++;
+//         j++; 
+//     }
+//     cout<<endl;
+//     ch++;
+//     i++;  
+// }
+
+//10).
+
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=n-1;
+//     int k=1;
+//     while(j>=i){
+//         cout<<" ";
+//         j--; 
+//     }
+//     while(k<=i){
+//         cout<<"*";
+//         k++; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//11).
+// int n;
+// cout<<"Enter the number of row:"<<endl;
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=n;
+//     int k=2;
+//     while(k<=i){
+//         cout<<" ";
+//         k++; 
+//     }
+//     while(j>=i){
+//         cout<<"*";
+//         j--; 
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+//12).
+
+// int n;
+// cout<<"Enter the number of row:";
+// cin>>n;
+
+// int i=1;
+// while(i<=n){
+//     int j=n-1;
+//     int k=1;
+//     int m=i;
+//     int l=2;
+//     while(j>=i){
+//         cout<<" ";
+//         j--; 
+//     }
+//     while(k<=i){
+//         cout<<k;
+//         k++; 
+//     }
+//     while (l<=i)
+//     {
+//         cout<<m-1;
+//         m--;
+//         l++;
+//     }
+//     cout<<endl;
+//     i++;  
+// }
+
+
+
 }
