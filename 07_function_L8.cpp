408ea20976124be1f0eb7891419626ab6076057a
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-07-25 00:47:52 +0530

    addd nCr

--- 07_function_L8.cpp
+++ 07_function_L8.cpp
@@ -1,20 +1,83 @@
 #include<iostream>
 using namespace std;
 
-int power(int a,int b){
-    int ans = 1;
-    for (int  i = 0; i < b;i++)
-    {
-        ans = ans * a;
-    }
-    return ans;
+// int power(int a,int b){
+//     int ans = 1;
+//     for (int  i = 0; i < b;i++)
+//     {
+//         ans = ans * a;
+//     }
+//     return ans;
     
-}
+// }
 
-int main (){
+// int main (){
 
-int a,b;
-cin>>a>>b;
-cout<<power(a,b);
-    return 0;
-}
+// int a,b;
+// cin>>a>>b;
+// cout<<power(a,b);
+//     return 0;
+// }
+
+// int power(){
+//     int a,b;
+//     cin>>a>>b;
+//     int ans = 1;
+//     for (int i=0;i<b;i++)
+//     {
+//         ans = ans * a;
+//     }
+//     return ans;
+    
+// }
+
+// int main (){
+// cout<<power();
+//     return 0;
+// }
+
+// Even / odd
+
+// bool isEven (int a){
+//     if(a&1){//odd
+//         return 0;
+//     }
+//     else{//even
+//         return 1;
+//     }
+// }
+// int main(){
+//     int num;
+//     cin >> num;
+
+//     if(isEven(num)){
+//         cout<<" Number is Even" << endl;
+//     }
+//     else{
+//         cout<<" Number is Odd" << endl;
+//     }
+//}
+
+//nCr calculating
+
+// int facto(int n){
+//     int fact = 1;
+//     for(int i = 1; i<=n; i++){
+//         fact = fact * i;
+//     }
+//     return fact;
+// }
+
+// int nCr(int n, int r){
+//     int num = facto(n);
+//     int dem = facto(r) * facto(n-r);
+
+//     return num/dem;
+// }
+// int main (){
+//     int n,r;
+//     cin>>n>>r;
+//     cout<<nCr(n,r);
+//     return 0;
+// }
+
