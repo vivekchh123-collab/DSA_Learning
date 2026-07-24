8f70a4f54f1500a3658e7c5b95597abb0935bfb3
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-07-24 21:29:57 +0530

    conver decimal number

--- 05_L6.cpp
+++ 05_L6.cpp
@@ -5,19 +5,41 @@ using namespace std;
 int main(){
 // 1). convert number into binary
 
-    int n;
-    cin>> n;
+    // int n;
+    // cin>> n;
 
-    int ans = 0;
-    int i = 0;
-    while (n != 0){
+    // int ans = 0;
+    // int i = 0;
+    // while (n != 0){
 
-        int bit = n & 1;
+    //     int bit = n & 1;
 
-        ans = (bit * pow(10,i)) + ans;
-        n = n >> 1;
-        i++;
-    }
+    //     ans = (bit * pow(10,i)) + ans;
+    //     n = n >> 1;
+    //     i++;
+    // }
 
-    cout<<"Answer is :"<< ans << endl;
+    // cout<<"Answer is :"<< ans << endl;
+
+//2).binary number into decimal number
+
+// int n;
+// cout<<"Enter the number:";
+// cin>>n;
+
+// int i =0;
+// int ans =0;
+// while (n!=0)
+// {
+//     int digit = n%10;
+//     if(digit ==1){
+//         ans = ans + pow(2,i);
+//     }
+//     n=n/10;
+//     i++;
+// }
+// cout << ans <<endl;
+
+
+
 }
