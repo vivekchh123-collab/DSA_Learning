ea7271f9382adb3adcc89e0be6d02ccbebd51d48
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-08-02 15:46:33 +0530

    update L_19

--- Lecture_11-20/19_STL.cpp
+++ Lecture_11-20/19_STL.cpp
@@ -178,46 +178,169 @@
 
 //Priority Queue
 
+// #include<iostream>
+// #include<queue>
+
+// using namespace std;
+// int main(){
+//     //max heap
+//     priority_queue<int> maxi;
+
+//     //min heap
+//     priority_queue<int,vector<int>,greater<int> > mini;
+
+//     maxi.push(1);
+//     maxi.push(3);
+//     maxi.push(2);
+//     maxi.push(0);
+
+//     cout<<"size->"<<maxi.size()<<endl;
+
+//     int n = maxi.size();
+
+//     for(int i = 0;i<n;i++){
+//         cout<<maxi.top()<<" ";
+//         maxi.pop();
+//     }
+//     cout<<endl;
+
+//     mini.push(1);
+//     mini.push(3);
+//     mini.push(2);
+//     mini.push(0);
+
+//     cout<<"size->"<<mini.size()<<endl;
+
+//     int m = mini.size();
+
+//     for(int i = 0;i<m;i++){
+//         cout<<mini.top()<<" ";
+//         mini.pop();
+//     }
+//     cout<<endl;
+
+//     cout<<"Empty or not "<<mini.empty()<<endl;
+// }
+
+//Set store unique numberr
+
+// #include<iostream>
+// #include<set>
+
+// using namespace std;
+// int main(){
+//     set<int> s;
+
+//     s.insert(5);
+//     s.insert(5);
+//     s.insert(1);
+//     s.insert(6);
+//     s.insert(7);
+//     s.insert(0);
+//     s.insert(5);
+//     s.insert(6);
+//     s.insert(0);
+//     s.insert(0);
+//     s.insert(5);
+
+//     for(auto i : s){
+//         cout<<i<<endl;
+//     }
+//     cout<<endl;
+
+//     set<int>:: iterator it = s.begin();
+//     it++;
+
+//     s.erase(it);
+
+//     for(auto i : s){
+//         cout<<i<<endl;
+//     }
+//     cout<<endl;
+
+//     cout<<"-5 is present or not ->"<<s.count(-5)<<endl;
+
+//     set<int>:: iterator itr = s.find(5);
+//     for(auto it=itr; it!=s.end();it++){
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+
+//Map
+
+// #include<iostream>
+// #include<map>
+
+// using namespace std;
+// int main(){
+//     map<int,string> m;
+
+//     m[1]="Babber";
+//     m[2]="Love";
+//     m[13]="Kumar";
+    
+//     m.insert({5,"Bheem"});
+
+//     cout<<"Before erase"<<endl;
+//     for(auto i:m){
+//         cout<<i.first<<" "<<i.second<<endl;
+//     }
+//     cout<<"finding -13-->"<<m.count(-13)<<endl;
+
+//     //m.erase(13);
+//     cout<<"after erase"<<endl;
+//     for(auto i:m){
+//         cout<<i.first<<" "<<i.second<<endl;
+//     }
+//     cout<<endl<<endl;
+
+//     auto it = m.find(5);
+
+//     for(auto i=it; i!=m.end();i++){
+//         cout<<(*i).first<<endl;
+
+//     }
+// }
+
+//Algoritmn
+
 #include<iostream>
-#include<queue>
+#include<algorithm>
+#include<vector>
 
 using namespace std;
 int main(){
-    //max heap
-    priority_queue<int> maxi;
+    vector<int> v;
 
-    //min heap
-    priority_queue<int,vector<int>,greater<int> > mini;
 
-    maxi.push(1);
-    maxi.push(3);
-    maxi.push(2);
-    maxi.push(0);
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
 
-    cout<<"size->"<<maxi.size()<<endl;
+    // cout<<"Find 6 "<<binary_search(v.begin(),v.end(),6)<<endl;
 
-    int n = maxi.size();
+    // cout<<"lower bound " <<lower_bound(v.begin(),v.end(),6)-v.begin()<<endl;
+    // cout<<"upper bound " <<upper_bound(v.begin(),v.end(),4)-v.begin()<<endl;
 
-    for(int i = 0;i<n;i++){
-        cout<<maxi.top()<<" ";
-        maxi.pop();
+    // int a=3;
+    // int b=5;
+
+    // cout<<"Max ->"<<max(a,b)<<endl;
+    // cout<<"Min ->"<<min(a,b)<<endl;
+
+    // swap(a,b);
+    // cout<<endl<<"a-> "<<a<<endl;
+
+    // string abcd = "abcd";
+    // reverse(abcd.begin(),abcd.end());
+    // cout<<"string-> "<< abcd<<endl;
+
+    rotate(v.begin(),v.begin()+1,v.end());
+    cout<<"after rotate" << endl;
+    for(int i:v){
+        cout<<i<<" ";
     }
-    cout<<endl;
-
-    mini.push(1);
-    mini.push(3);
-    mini.push(2);
-    mini.push(0);
-
-    cout<<"size->"<<mini.size()<<endl;
-
-    int m = mini.size();
-
-    for(int i = 0;i<m;i++){
-        cout<<mini.top()<<" ";
-        mini.pop();
-    }
-    cout<<endl;
-
-    cout<<"Empty or not "<<mini.empty()<<endl;
+   
 }
