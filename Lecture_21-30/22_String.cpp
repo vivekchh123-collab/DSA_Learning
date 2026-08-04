56f4adf1be6256ced5d9f0004013e981df551cce
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-08-04 16:09:17 +0530

    update lect

--- Lecture_21-30/22_String.cpp
+++ Lecture_21-30/22_String.cpp
@@ -2,39 +2,115 @@
 using namespace std;
 
 //find the length of string
-int getLength(char name[]){
-    int count=0;
-    for(int i=0;name[i]!='\0';i++){
-        count++;
-    }
-    return count;
- }
+// int getLength(char name[]){
+//     int count=0;
+//     for(int i=0;name[i]!='\0';i++){
+//         count++;
+//     }
+//     return count;
+//  }
  //Reverse a string...
-void reverse(char name[], int n){
-    int s=0;
+void reverse(string &name, int k,int n){
+    int s=k;
     int e=n-1;
     while (s<e){
 swap(name[s++],name[e--]);
-    }
-    
-    
+    }   
 }
 
+// introdunction to string in c++
+// int main(){
+    
+//      char  name[20];
+
+//      cout << "Enter your name:"<<endl;
+//      cin >> name;
+
+//     cout<<"Your name is:";
+//     cout<<name<<endl;
+//      int len = getLength(name);
+//     cout<< "Length:" << len << endl;
+//     reverse(name,len);
+//     cout << "Reverse is:" << name <<endl;
+
+// return 0; 
+//  }
+
+
+ //valid palidrome
+
+//  class Solution {
+//     private: 
+//     bool valid(char ch){
+//         if((ch>='a' && ch <= 'z') || (ch>= 'A' && ch<= 'Z') || (ch>= '0' && ch<= '9') ){
+//             return 1;
+//         }
+//         return 0;
+//     }
+//    char toLowerCase(char ch){
+//     if((ch>='a' && ch <= 'z') || (ch>= '0' && ch<= '9')){
+//         return ch;
+//     }
+//     else{
+//         char temp = ch -'A' + 'a';  //change to lower case
+//         return temp;
+//     }
+//    }
+//    bool checkPalidrome(string a){
+//     int s=0;
+//     int e =a.length() -1;
+
+//     while(s<=e){
+//         if(a[s] != a[e]){
+//             return 0;
+//         }
+//         else{
+//             s++;
+//             e--;
+//         }
+//     }
+
+//     return 1;
+//    }
+
+
+// public:
+//     bool isPalindrome(string s) {
+
+//         //ignore unused character
+//         string temp="";
+//         for(int j =0;j<s.length();j++){
+//             if(valid(s[j])){
+//                 temp.push_back(s[j]);
+//             }
+//         }
+
+//         //lower written
+//         for(int j =0;j<temp.length();j++){
+//             temp[j] = toLowerCase(temp[j]);
+//         }
+// // check Palidrome
+//         return checkPalidrome(temp);
+        
+//     }
+// };
+
+
+//Reverse An aman in love to  nA nama ni evol
 
 int main(){
-    
-     char  name[20];
+    string name = "An man in love";
+    int n = name.length();
+    int start = 0;
 
-     cout << "Enter your name:"<<endl;
-     cin >> name;
+    for (int i = 0; i <= n; i++) {
+        if (i == n || name[i] == ' ') {
+            reverse(name, start, i);
+            start = i + 1;
+        }
+    }
 
-    cout<<"Your name is:";
-    cout<<name<<endl;
-     int len = getLength(name);
-    cout<< "Length:" << len << endl;
-    reverse(name,len);
-    cout << "Reverse is:" << name <<endl;
-
-return 0; 
- }
+    cout << name << endl;
+    return 0;
+}
 
