31e2aeeba064b1e45f69f772f41442a2ce4bb586
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-08-13 19:23:34 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Lecture_21-30/25_Pointer.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    int a = num;
+    cout << "a before " << num << endl;
+    a++;
+    cout << "a after " << num << endl;
+
+    int *p = &num;
+    cout << "before " << num << endl;
+    (*p)++;
+    cout << "after " << num << endl;
+
+    //copying a pointer
+    int *q = p;
+    cout << p <<" - " << q << endl;
+    cout << *p <<" - " << *q << endl;
+
+    //important concept
+    int i = 3;
+    int *t = &i;
+    cout << *t++ << endl;
+
+    return 0;
+}
---
+++ Lecture_21-30/26_pointerInfo.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int arr[10] = {2,5,4};
+
+    // cout <<" address of first memory block is " << arr << endl;
+    // cout << arr[0] << endl;
+    // cout <<" address of first memory block is " << &arr[0] << endl;
+
+    // cout<< "value of indexx"<< *arr << endl;
+    // cout<< "increase one by at zeroth location:"<< *arr +1 << endl;
+    // cout<< "value of index 1:"<< *(arr+1) << endl;
+
+    // int i=2;
+    // cout<< i[arr] << endl;
+
+    
+
+
+    return 0;
+}
