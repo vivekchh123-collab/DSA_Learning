d8bc7b056632bb80ba8e9f57a629d3124261db13
Vivek Chaurasiya <vivekchh123@gmail.com>
2026-08-13 19:33:17 +0530

    Add pointer size calculations and update binary executable

--- Lecture_21-30/26_pointerInfo.cpp
+++ Lecture_21-30/26_pointerInfo.cpp
@@ -16,8 +16,19 @@ int main () {
     // int i=2;
     // cout<< i[arr] << endl;
 
-    
+    int temp[10];
+    cout<< sizeof(temp) << endl;
+    cout<< sizeof(*temp) << endl;
+    cout<< sizeof(&temp) << endl;
+
+    int *ptr = &temp[10];
+    cout<< sizeof(ptr) << endl;
+    cout <<sizeof(*ptr) << endl;
+    cout <<sizeof(&ptr) << endl;
 
 
-    return 0;
+
+
+
+   return 0;
 }
